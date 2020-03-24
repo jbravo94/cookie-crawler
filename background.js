d3c0bf2c3fb1404d131fb8736814f7d18d295b85
Johannes Heinzl <jh.heinzl@gmail.com>
2020-03-25 00:07:35 +0100

    Dev.

--- background.js
+++ background.js
@@ -1,5 +1,25 @@
 chrome.browserAction.onClicked.addListener(function(tab) { 
-  chrome.cookies.getAll({name: "crowd.token_key"}, function (cookies) {
-    alert(JSON.stringify(cookies));
+  chrome.cookies.getAll({domain: "celix.at", name: "crowd.token_key"}, function (cookies) {
+    
+if (!cookies || cookies.length === 0 || !cookies[0] || !cookies[0].value) {
+    alert("Could not find cookie!");
+}
+
+var crowdtokenkey = cookies[0].value;
+
+//alert(crowdtokenkey);
+
+    fetch('http://localhost:33457/setcrowdtokenkey', {
+    method: 'post',
+    headers: {
+       'Content-Type': 'application/json',
+       'Authorization': 'Basic ' + btoa("chromeext" + ":" + "supersecret")
+    },
+    body: JSON.stringify({crowdtokenkey: crowdtokenkey})
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    alert(JSON.stringify(data));
+  }).catch(function(err){alert("Error occured: " + JSON.stringify(err));});
   });
 });
